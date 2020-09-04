8701af0857dd01fda527b75286752a0d67d4d58d
Antonia von den Driesch <antonia@giantswarm.io>
2020-09-04 19:37:55 +0900

    bump version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-collector"
 	source      string = "https://github.com/giantswarm/aws-collector"
-	version            = "0.0.1-dev"
+	version            = "1.0.0"
 )
 
 func Description() string {
